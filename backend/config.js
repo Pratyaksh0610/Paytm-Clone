1115700ebe3144fc2732b939b070636018370e5b
Pratyaksh <pratyaksh0610@gmail.com>
2024-02-25 23:43:46 +0530

    Done with all User Routes

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,2 @@
 const JWT_SECRET = "MySecret";
-module.exports = {
-  JWT_SECRET,
-};
+module.exports = JWT_SECRET;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,8 @@ const express = require("express");
 const zod = require("zod");
 const { User } = require("../db");
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../config");
+const JWT_SECRET = require("../config");
+const authMiddleware = require("./middleware");
 const app = express();
 console.log("Inside userRouter");
 
@@ -59,7 +60,7 @@ userRouter.post("/signin", async function (req, res) {
   if (existingUser) {
     let id = existingUser[0]._id;
     console.log(id);
-    const token = jwt.sign({ id: id }, JWT_SECRET);
+    const token = jwt.sign({ userId: id }, JWT_SECRET);
     res.status(200).json({
       token: token,
     });
@@ -68,6 +69,36 @@ userRouter.post("/signin", async function (req, res) {
       msg: "Error while logging in",
     });
   }
+});
+
+const updateBody = zod.object({
+  password: zod.string().optional(),
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+});
+
+userRouter.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateBody.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({
+      message: "Error while updating information",
+    });
+  }
+
+  await User.updateOne({ _id: req.userId }, req.body);
+
+  res.json({
+    message: "Updated successfully",
+  });
+});
+
+userRouter.get("/bulk", async function (req, res) {
+  const filter = req.query.filter || "";
+  const lName = req.query.filter || "";
+  const userList = await User.find({
+    $or: [{ firstName: fName }, { lastName: lName }],
+  });
+  res.status(200).json(userList);
 });
 
 module.exports = {
