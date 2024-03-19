71807db5478f5cb31c7b6d21d52c1414942a901a
Pratyaksh <pratyaksh0610@gmail.com>
2024-03-20 00:12:34 +0530

    Added Logout

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -46,10 +46,10 @@ const Modal = ({ isOpen, onClose, children }) => {
           onChange={(e) => setAmount(e.target.value)}
         />
         <button
-          class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 w-8/12"
+          className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 w-8/12"
           onClick={post}
         >
-          <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full">
+          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 w-full">
             Transfer
           </span>
         </button>
@@ -66,48 +66,90 @@ const Modal = ({ isOpen, onClose, children }) => {
 };
 let megaUserId = null;
 export function Dashboard() {
-  const [loading, setLoading] = useState(true);
-  const [userBalance, setuserBalance] = useState(null);
-  const [userData, setuserData] = useState(null);
+  // const [loading, setLoading] = useState(true);
+  // const [userBalance, setuserBalance] = useState(null);
+  // const [userData, setuserData] = useState(null);
   // const [userId,setuserId]=useState(0);
+  //diff
+  const [loading, setLoading] = useState(true);
+  const [userBalance, setUserBalance] = useState(null);
+  const [userData, setUserData] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const cookieValue = Cookies.get("jwtoken");
-    console.log("WORKING ONCE");
-    try {
-      axios
-        .post("http://localhost:3000/api/v1/user/resolveToken/", {
-          token: cookieValue,
-        })
-        .then((res) => {
-          if (res.status == 200) {
-            console.log("RECEVIED");
-            console.log(res.data);
-            setuserData(res.data);
-            megaUserId = res.data.userId;
-          } else {
-            console.log("FAILED");
-            navigate("/signin");
+    const fetchData = async () => {
+      const cookieValue = Cookies.get("jwtoken");
+      try {
+        const resolveTokenResponse = await axios.post(
+          "http://localhost:3000/api/v1/user/resolveToken/",
+          {
+            token: cookieValue,
           }
-        });
-      const headers = {
-        Authorization: "Bearer " + cookieValue,
-      };
-      axios
-        .get("http://localhost:3000/api/v1/account/balance", { headers })
-        .then((res) => {
-          setuserBalance(res.data.balance);
-          console.log(res.data.balance);
+        );
+        if (resolveTokenResponse.status === 200) {
+          setUserData(resolveTokenResponse.data);
+          const headers = { Authorization: "Bearer " + cookieValue };
+          const balanceResponse = await axios.get(
+            "http://localhost:3000/api/v1/account/balance",
+            { headers }
+          );
+          setUserBalance(balanceResponse.data.balance);
           setLoading(false);
-        });
-    } catch (error) {
-      navigate("/signin");
-    }
-  }, []);
+        } else {
+          navigate("/signin");
+        }
+      } catch (error) {
+        navigate("/signin");
+      }
+    };
+
+    fetchData();
+  }, [navigate]);
+  // useEffect(() => {
+  //   const cookieValue = Cookies.get("jwtoken");
+  //   console.log("WORKING ONCE");
+  //   try {
+  //     axios
+  //       .post("http://localhost:3000/api/v1/user/resolveToken/", {
+  //         token: cookieValue,
+  //       })
+  //       .then((res) => {
+  //         if (res.status == 200) {
+  //           console.log("RECEVIED");
+  //           console.log(res.data);
+  //           setuserData(res.data);
+  //           megaUserId = res.data.userId;
+  //         } else {
+  //           console.log("FAILED");
+  //           navigate("/signin");
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //         navigate("/signin");
+  //       });
+  //     const headers = {
+  //       Authorization: "Bearer " + cookieValue,
+  //     };
+  //     axios
+  //       .get("http://localhost:3000/api/v1/account/balance", { headers })
+  //       .then((res) => {
+  //         setuserBalance(res.data.balance);
+  //         console.log(res.data.balance);
+  //         setLoading(false);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //         navigate("/sigin");
+  //       });
+  //   } catch (error) {
+  //     navigate("/signin");
+  //   }
+  // }, []);
   if (loading) {
     return <div>Loading...</div>;
   }
+
   return (
     <>
       <div>
@@ -115,16 +157,35 @@ export function Dashboard() {
           <div>
             <h1 className="text-2xl font-bold text-black">Payments App</h1>
           </div>
-          <div>
-            <h2 className="text-lg font-semibold">
+          <div className="flex justify-between items-center">
+            <span className="text-lg font-semibold mr-2 middle ">
               Hello, {userData.firstName}
-            </h2>
+            </span>
+            <div
+              className="w-10 h-10 flex items-center justify-center rounded-full text-white font-semibold text-lg"
+              style={{ backgroundColor: randomColor() }}
+            >
+              {userData.firstName.charAt(0)}
+            </div>
           </div>
         </div>
         <hr className="border-b-2 border-gray-400" />
-        <div className="font-bold text-md flex justify-start my-4">
-          <span className="pr-2">Your Balance</span>
-          <span>${userBalance}</span>
+        <div className="flex justify-between">
+          <div className="font-bold text-md flex justify-start my-4">
+            <span className="pr-2">Your Balance</span>
+            <span>${userBalance}</span>
+          </div>
+          <button
+            type="button"
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-4 my-2 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            onClick={() => {
+              Cookies.set("jwtoken", "fhadsfksjf");
+              setLoading(true);
+              navigate("/signin");
+            }}
+          >
+            Logout
+          </button>
         </div>
         <Userlist />
       </div>
